b3b53d69f9e188568d636d93373cf928c2b55899
Ashutosh <ashuhina.123@gmail.com>
2024-05-23 14:27:07 +0530

    Memoized the InsertNode

--- sidebar/src/hooks/use-traverse-tree.js
+++ sidebar/src/hooks/use-traverse-tree.js
@@ -1,34 +1,52 @@
-const useTraverseTree = ()=> {
+const useTraverseTree = () => {
 
-    function insertNode(tree,folderId,item,isFolder){
-        if(tree.id === folderId && tree.isFolder){
-            tree.items.unshift({
-                id: new Date().getTime(),
-                name: item,
-                isFolder,
-                items:[]
-            })
-            return tree;
+    const insertNode = (() => {
+
+        const dp = new Map();
+
+        return (tree, folderId, item, isFolder) => {
+
+            const dpKey = `${tree.id}+${folderId}+${item}+${isFolder}`;
+
+            if (dp.has(dpKey)) {
+                return dp.get(dpKey);
+            }
+
+            if (tree.id === folderId && tree.isFolder) {
+                tree.items.unshift({
+                    id: new Date().getTime(),
+                    name: item,
+                    isFolder,
+                    items: []
+                })
+                dp.set(dpKey, tree);
+                return tree;
+            }
+
+            let latestNode = tree.items.map((ele) => (
+                insertNode(ele, folderId, item, isFolder)
+            ));
+            const ans = { ...tree, items: latestNode }
+            dp.set(dpKey, ans);
+            return ans
         }
 
-        let latestNode = tree.items.map((ele) => (
-            insertNode(ele,folderId,item,isFolder)
-        ));
+    })();
 
-        return {...tree, items: latestNode}
-    }
+    function editNode(tree, folderId, name) {
 
-    function editNode(tree,folderId,name){
-        if(tree.id === folderId){
+        if (tree.id === folderId) {
             tree.name = name;
             return tree;
         }
 
-        const latestNode = tree.items.map((ele) => editNode(ele,folderId,name));
-        return {...tree, items: latestNode}
+        const latestNode = tree.items.map((ele) => editNode(ele, folderId, name));
+        const ans = { ...tree, items: latestNode }
+
+        return ans;
     }
 
-    function deleteNode(tree, folderId) {
+    function deleteNode (tree, folderId) {
         if (tree.items) {
             tree.items = tree.items.filter(item => item.id !== folderId);
             tree.items.forEach(item => deleteNode(item, folderId));
@@ -42,7 +60,7 @@ const useTraverseTree = ()=> {
         return newTree;
     }
 
-    return {insertNode, deleteTheFuckingNode, editNode}
+    return { insertNode, deleteTheFuckingNode, editNode }
 
 }
 
